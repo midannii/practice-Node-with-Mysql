2f9c507e41aabf17347e5229a2dd343e5708ee8b
midannii <48229457+midannii@users.noreply.github.com>
2020-11-05 09:29:47 +0900

    update querys

--- codes/node-mysql/db.js
+++ codes/node-mysql/db.js
@@ -4,18 +4,22 @@ const conn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'mypassword',
-  database : 'mydatabase'
+  database : 'midan'
 });
 conn.connect();
+
+if (conn){
+  console.log('connect success!')
+};
 
 // select all
 var sql = 'SELECT * from mylogs';
 conn.query(sql, function(err, row, fields) {
-  if (error){
-    console.log(err);
-  }else{
-    console.log('rows', row);
-    console.log('fields', fields);
+   try{
+    console.log('rows: ', row);
+    console.log('fields: ', fields);
+  } catch(e){
+    console.log(e)
   }
 
 })
@@ -23,65 +27,68 @@ conn.query(sql, function(err, row, fields) {
 // select all & for loop
 var sql = 'SELECT * from mylogs';
 conn.query(sql, function(err, rows, fields) {
-  if (error){
-    console.log(err);
-  }else{
+  try{
+    console.log('SELECT data ')
     for(var i = 0; i<rows.length; i++){
-      console.log(rows[i].title);
+      console.log(rows[i]);
+    }} catch(e){
+      console.log(e);
     }
-  }
 
 })
 
 // insert
-var sql = 'INSERT INTO mylogs (title, description, date) VALUES ("first day", "today is first day of this logs", NOW())';
+var sql = 'INSERT INTO mylogs (title, description, dates) VALUES ("first day 2", "today is first day of this logs", "2020-11-03 23:40:50")';
 
 conn.query(sql, function(err, rows, fields){
-  if (error) {
+  try {
+    console.log('INSERT data ')
+    console.log(rows);
+  } catch (e) {
     console.log(err);
-  } else {
-    console.log(rows.title);
   }
 })
 
 // insert with using params
 
-var sql = 'INSERT INTO mylogs (title, description, date) VALUES (?, ?, ?)';
-var params = ["first day", "today is first day of this logs", NOW()];
+var sql = 'INSERT INTO mylogs (title, description, dates) VALUES (?, ?, ?)';
+var params = ["first day", "today is first day of this logs", '2020-11-04 00:05:10'];
 
 conn.query(sql, params, function(err, rows, fields){
-  if (error) {
-    console.log(err);
-  } else {
-    console.log(rows.title);
+  try{
+    console.log('INSERT data ')
+    console.log(rows);
+  } catch (e) {
+    console.log(e);
   }
 })
 
 // update
 
 var sql = 'UPDATE mylogs SET title = ?, description = ? WHERE id = ?';
-var params = ["yesterday", "tomorrow i'm gonna make my db", 1];
+var params = ["yesterday", "tomorrow i'm gonna make my db", 3];
 
 conn.query(sql, params, function(err, rows, fields){
-  if (error) {
-    console.log(err);
-  } else {
-    console.log(rows.title);
+  try{
+    console.log('UPDATE id = 3')
+    console.log(rows);
+  } catch(e) {
+    console.log(e);
   }
 })
 
 // delete
 
 var sql = 'DELETE FROM mylogs where id = ?';
-var params = [1];
+var params = [2];
 
 conn.query(sql, params, function(err, rows, fields){
-  if (error) {
-    console.log(err);
-  } else {
-    console.log(rows.title);
+  try {
+    console.log('Delete id = 2')
+    console.log(rows);
+  } catch(e) {
+    console.log(e);
   }
 })
-
 
 conn.end();
--- codes/node-mysql/mybd.sql
+++ codes/node-mysql/mybd.sql
@@ -2,10 +2,13 @@ CREATE DATABASE midan;
 
 use midan;
 
-CREATE TABLE 'mylogs' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'title' varchar(100) NOT NULL,
-  'description' text NOT NULL,
-  'date' DATETIME NOT NULL,
+CREATE TABLE mylogs (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  title varchar(100) NOT NULL,
+  description text NOT NULL,
+  dates DATETIME NOT NULL,
   PRIMARY KEY (id)
 );
+
+insert into mylogs (title, description, dates) values ('November 5th', '2 months left in 2020', NOW());
+insert into mylogs (title, description, dates) values ('November 6th', 'I think winter is come in KOREA', NOW());
