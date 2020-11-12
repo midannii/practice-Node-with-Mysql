b0d00d2ee0d5e1d4eee0840029a6ca212567e4dd
midannii <48229457+midannii@users.noreply.github.com>
2020-11-12 13:52:38 +0900

    Update index.js
    connect with mongoDB

--- codes/boiler_plates/index.js
+++ codes/boiler_plates/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// connect with mongoDB
+// https://www.youtube.com/watch?v=TTmfGULw0Uw&list=PL9a7QRYt5fqly7BrCxOS71BqLLb9OeXKd&index=2
+mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true}).then(() => console.log('DB connected'))
+                          .catch(err => console.error(err));
 
 app.get('/', (req,res) => {
   res.send('hello world')
