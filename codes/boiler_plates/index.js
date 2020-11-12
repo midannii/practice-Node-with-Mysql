ce9290f9a09d4f333edde3e335eaac3b9be4d1cc
midannii <48229457+midannii@users.noreply.github.com>
2020-11-12 14:40:43 +0900

    codes with postman

--- codes/boiler_plates/index.js
+++ codes/boiler_plates/index.js
@@ -2,11 +2,33 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 // connect with mongoDB
 // https://www.youtube.com/watch?v=TTmfGULw0Uw&list=PL9a7QRYt5fqly7BrCxOS71BqLLb9OeXKd&index=2
 mongoose.connect('mongodb://localhost/my_database', {
   useNewUrlParser: true}).then(() => console.log('DB connected'))
                           .catch(err => console.error(err));
+
+
+  //to not get any deprecation warning or error
+  //support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// add info with postman: https://www.youtube.com/watch?v=FW7MfF4RDjg&list=PL9a7QRYt5fqly7BrCxOS71BqLLb9OeXKd&index=6
+app.post('/api/users/register', (req,res)=> {
+  const user = new User(req.body)
+
+  user.save((err, userData) => {
+    if(err) return res.json({success: false, err});
+    return res.status(200);
+  })
+})
 
 app.get('/', (req,res) => {
   res.send('hello world')
